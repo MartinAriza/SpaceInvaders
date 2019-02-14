69057a2959306488f9ea45d2fcdf6d183e648d62
Daoab <38359000+Daoab@users.noreply.github.com>
2019-02-14 09:12:13 +0100

    Añadidas particulas del disparo del alien

--- Space Invaders/Assets/Scripts/Alien.cs
+++ Space Invaders/Assets/Scripts/Alien.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Alien : MonoBehaviour
 {
+    public float minTimeBetweenShots = 1.0f;
+    public float maxTimeBetweenShots = 3.0f;
+
+    bool stopFiring = false;
+
     void Start()
     {
         gameObject.GetComponent<CubeEditor>().enabled = false; //Se desactiva el snap para que se puedan mover en el play
+        StartCoroutine(fire());
     }
 
-    void Update()
+    IEnumerator fire()
     {
-
+        while(!stopFiring)
+        {
+            yield return new WaitForSeconds( Random.Range(minTimeBetweenShots, maxTimeBetweenShots) );
+        }
     }
 }
