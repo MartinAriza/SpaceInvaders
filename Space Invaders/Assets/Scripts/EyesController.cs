a9a2a2ce5653296e883f2bc422cbbc0495971c20
Pedro Casas Martínez <pedrocasas876@gmail.com>
2019-02-16 19:56:34 +0100

    Añadidos prefabs de los tres tipos de alien. Tienen su animator controller, script de movimiento de ojos y collider.

---
+++ Space Invaders/Assets/Scripts/EyesController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyesController : MonoBehaviour
+{
+
+    private Vector3 originalLook;
+    [SerializeField] string targetName;
+    [SerializeField] GameObject target;
+    float maxAngle = 50.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalLook = transform.forward;
+
+        //Si no se ha introducido un nombre en el inspector, se usa "Player" por defecto
+        if (targetName == "") targetName = "Player";
+
+        if (!target) target = GameObject.Find(targetName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 actualLook = transform.forward;
+        transform.LookAt(target.transform);
+        float actualAngle = Vector3.Angle(originalLook, transform.forward);
+        //print(actualAngle);
+        if (actualAngle > maxAngle)
+        {
+            transform.LookAt(transform.position + actualLook);
+            //print("blocking");
+        }
+    }
+}
