492b07d1ce3f7a3804d32873493c3eef4432e936
nesherben <nestor8299@gmail.com>
2019-02-18 16:35:34 +0100

    shadder y menu

---
+++ Space Invaders/Assets/Scripts/botones.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class botones : MonoBehaviour,
+        IPointerClickHandler // 2
+      , IDragHandler
+      , IPointerEnterHandler
+      , IPointerExitHandler
+{
+
+    public Sprite sprite_0;
+    public Sprite sprite_ex;
+
+    public void OnPointerClick(PointerEventData eventData) // 3
+    {
+        print("I was clicked");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        print("I'm being dragged!");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GetComponent<Image>().sprite = sprite_ex;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GetComponent<Image>().sprite = sprite_0;
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Space Invaders/Assets/Scripts/empezar_game.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class empezar_game : MonoBehaviour, IPointerClickHandler
+{
+   
+    public void OnPointerClick(PointerEventData eventData) // 3
+    {
+        SceneManager.LoadScene("GAME");
+    }
+
+}
