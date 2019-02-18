ff2985cd82c9bb9b6b960cd27881f5f0bb6556a8
nesherben <nestor8299@gmail.com>
2019-02-18 15:30:09 +0100

    menu

---
+++ Space Invaders/Assets/Scripts/exit_game.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class exit_game : MonoBehaviour, IPointerClickHandler
+{
+
+    // Use this for initialization
+    public void OnPointerClick(PointerEventData eventData) // 3
+    {
+        Application.Quit();
+    }
+}
---
+++ Space Invaders/Assets/Scripts/ir_scores.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ir_scores : MonoBehaviour, IPointerClickHandler
+{
+
+    public void OnPointerClick(PointerEventData eventData) // 3
+    {
+        SceneManager.LoadScene("Scoreboard");
+    }
+}
