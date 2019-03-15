68d8a04971bea77652dcb8c3e6d76fefdbbaef08
DESKTOP-MD5IRL4\Iván <ivan-ss-99@hotmail.com>
2019-03-15 17:29:16 +0100

    añadida fuente olympic carrier

--- Space Invaders/Assets/Scripts/ScoreManager.cs
+++ Space Invaders/Assets/Scripts/ScoreManager.cs
@@ -5,10 +5,15 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    public float playerScore = 0.0f;    
+    public float playerScore = 0.0f;
+    public string playerName;
+    public InputField inp;
+    public Button backToMenu;
 
     Horde horde;
     [SerializeField] Text scoreText;
+    [SerializeField] Text setNameText;
+    [SerializeField] Text nameText;
 
     void start()
     {
@@ -23,8 +28,14 @@ public class ScoreManager : MonoBehaviour
         horde.speed = 0.0f;
         horde.stopFiring();
 
+        inp.gameObject.SetActive(true);
+        setNameText.gameObject.SetActive(true);
         scoreText.gameObject.SetActive(true);
         scoreText.text = "Puntos:  " + playerScore.ToString();
+        if(inp.text.Length == 3)
+        {
+            inp.interactable = false;
+        }
 
         if (horde.speed <= 0.0f) horde.speed = 0.0f;
     }
