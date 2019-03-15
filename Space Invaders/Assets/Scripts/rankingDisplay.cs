b117afcbbc70b918aedc79b729fc6d62c85a6de4
Pedro Casas Martínez <pedrocasas876@gmail.com>
2019-03-15 18:16:43 +0100

    Ranking en el menú principal funcional

--- Space Invaders/Assets/Scripts/rankingDisplay.cs
+++ Space Invaders/Assets/Scripts/rankingDisplay.cs
@@ -1,19 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class rankingDisplay : MonoBehaviour
 {
-    [SerializeField] GameObject textsParent;
+    [SerializeField] Text names;
+    [SerializeField] Text scores;
     private void OnEnable()
     {
-        if (!textsParent) textsParent = GameObject.Find("textsParent");
-        if (textsParent)
+        if (!names) names = GameObject.Find("names").GetComponent<Text>();
+        if (!scores) scores = GameObject.Find("scores").GetComponent<Text>();
+        if (names && scores)
         {
+            names.text = "";
+            scores.text = "";
             string[] lineas = rankingController.getRanking();
-            //GameObject texts = textsParent.getChi
-        }
-        
+            for(int i = 0; i < rankingController.maxPlayersInRanking; i++)
+            {
+                if (i < lineas.Length)
+                {
+                    names.text += lineas[i][0];
+                    names.text += lineas[i][1];
+                    names.text += lineas[i][2];
 
+                    for (int j = 3; j < lineas[i].Length; j++)
+                    {
+                        scores.text += lineas[i][j];
+                    }
+                    names.text += "\n";
+                    scores.text += "\n";
+                } else
+                {
+                    names.text += "---" + "\n";
+                    scores.text += "-----" + "\n";
+                }
+            }
+        }
     }
 }
