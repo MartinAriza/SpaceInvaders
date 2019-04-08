7920e743974785326d998711de8d6b203b5e5092
Daoab <38359000+Daoab@users.noreply.github.com>
2019-04-09 00:29:22 +0200

    Añadida base del pathfinding de los aliens!

---
+++ Space Invaders/Assets/Scripts/Pathfinding/Waypoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] float explorableDistance = 5.0f;
+    public ArrayList explorableWaypoints = new ArrayList();
+
+    [HideInInspector] public bool isExplored = false;
+    [HideInInspector] public Waypoint exploredFrom;
+
+    void Start()
+    {
+        findExplorableWaypoints();
+    }
+
+    private void findExplorableWaypoints()
+    {
+        Collider[] aux = Physics.OverlapSphere(transform.position, explorableDistance, LayerMask.GetMask("Waypoint"));
+
+        foreach(Collider collider in aux)
+        {
+            if(collider.gameObject.transform != gameObject.transform)
+                explorableWaypoints.Add(collider.gameObject.GetComponent<Waypoint>());
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(0f, 1f, 0f, 0.2f);
+        Gizmos.DrawSphere(transform.position, explorableDistance);
+    }
+}
