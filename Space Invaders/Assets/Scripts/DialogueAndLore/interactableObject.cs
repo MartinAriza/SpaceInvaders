7fcc938cd89ef05f8de668aa2222393eb7e10595
Daoab <38359000+Daoab@users.noreply.github.com>
2019-04-17 12:38:16 +0200

    Importado nuevo escenario y sus texturas

--- Space Invaders/Assets/Scripts/DialogueAndLore/interactableObject.cs
+++ Space Invaders/Assets/Scripts/DialogueAndLore/interactableObject.cs
@@ -6,15 +6,18 @@ using UnityEngine.Events;
 public class interactableObject : MonoBehaviour
 {
     public UnityEvent onClick;
+    Outline outline;
     int layerMask;
 
     private void Start()
     {
         layerMask = LayerMask.GetMask("Interactable");
+        outline = GetComponent<Outline>();
     }
 
     private void LateUpdate()
     {
+        checkMouseOver();
         if(Input.GetMouseButton(0))
         {
             RaycastHit hitInfo = new RaycastHit();
@@ -26,5 +29,15 @@ public class interactableObject : MonoBehaviour
                 }
             }
         }
+    }
+
+    private void checkMouseOver()
+    {
+        RaycastHit hitInfo = new RaycastHit();
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo, Mathf.Infinity, layerMask))
+        {
+            if (hitInfo.transform.gameObject.tag == "Interactable") outline.enabled = true;
+        }
+        else outline.enabled = false;
     }
 }
